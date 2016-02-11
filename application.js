3c8ad8158cc54c735ca498a1ce17a080c62ec628
Michael Pavling <pavling@gmail.com>
2016-02-11 09:40:58 +0000

    moving functions into object properties

--- application.js
+++ application.js
@@ -2,27 +2,66 @@ var calculator = calculator || {
   previousOperator: 0,
   previousTotal: 0,
   newTotal: 0,
+  runningTotal: document.getElementById('running_total'),
+
   add: function(number){
     return parseFloat(calculator.previousTotal) + parseFloat(number);
   },
+
   subtract: function(number){
     return parseFloat(calculator.previousTotal) - parseFloat(number);
   },
+
   multiply: function(number){
     return parseFloat(calculator.previousTotal) * parseFloat(number);
   },
+
   divide: function(number){
     return parseFloat(calculator.previousTotal) / parseFloat(number);
+  },
+
+  numberClick: function(number) {
+    if (calculator.runningTotal.value == '0' || calculator.newTotal) {
+      calculator.runningTotal.value = '';
+      calculator.newTotal = false;
+    }
+    calculator.runningTotal.value = calculator.runningTotal.value + number;
+  },
+
+  operatorClick: function(operator) {
+    if (calculator.previousTotal && calculator.previousOperator) {
+      switch (calculator.previousOperator) {
+        case ('+'):
+        calculator.runningTotal.value = calculator.add(calculator.runningTotal.value);
+        break;
+        case ('-'):
+        calculator.runningTotal.value = calculator.subtract(calculator.runningTotal.value);
+        break;
+        case ('*'):
+        calculator.runningTotal.value = calculator.multiply(calculator.runningTotal.value);
+        break;
+        case ('/'):
+        calculator.runningTotal.value = calculator.divide(calculator.runningTotal.value);
+        break;
+      }
+    }
+
+    if (operator == '=') {
+      calculator.previousOperator = null;
+    } else {
+      calculator.previousOperator = operator;
+    }
+    calculator.previousTotal = calculator.runningTotal.value;
+    calculator.newTotal = true;
   }
+
 };
 
-
 var numbers = document.getElementsByClassName('number');
-
 for (var i = numbers.length - 1; i >= 0; --i) {
   numbers[i].addEventListener('click', function(e) {
     number = e.srcElement.innerText;
-    numberClick(number);
+    calculator.numberClick(number);
   });
 };
 
@@ -30,58 +69,19 @@ var operators = document.getElementsByClassName('operator');
 for (var i = operators.length - 1; i >= 0; --i) {
   operators[i].addEventListener('click', function(e) {
     operator = (e.srcElement.innerText);
-    operatorClick(operator);
+    calculator.operatorClick(operator);
   });
 };
 
 document.getElementById('clear').addEventListener('click', function() {
-  var runningTotal = document.getElementById('running_total');
-
-  if (runningTotal.value == 0) {
+  if (calculator.runningTotal.value == 0) {
     calculator.previousOperator = null;
     calculator.previousTotal = null;
   }
-  runningTotal.value = '';
+  calculator.runningTotal.value = '';
 });
 
 
-function numberClick(number) {
-  var runningTotal = document.getElementById('running_total');
-  if (runningTotal.value == '0' || calculator.newTotal) {
-    runningTotal.value = '';
-    calculator.newTotal = false;
-  }
-  runningTotal.value = runningTotal.value + number;
-}
-
-function operatorClick(operator) {
-  var runningTotal = document.getElementById('running_total');
-
-  if (calculator.previousTotal && calculator.previousOperator) {
-    switch (calculator.previousOperator) {
-      case ('+'):
-      runningTotal.value = calculator.add(runningTotal.value);
-      break;
-      case ('-'):
-      runningTotal.value = calculator.subtract(runningTotal.value);
-      break;
-      case ('*'):
-      runningTotal.value = calculator.multiply(runningTotal.value);
-      break;
-      case ('/'):
-      runningTotal.value = calculator.divide(runningTotal.value);
-      break;
-    }
-  }
-
-  if (operator == '=') {
-    calculator.previousOperator = null;
-  } else {
-    calculator.previousOperator = operator;
-  }
-  calculator.previousTotal = runningTotal.value;
-  calculator.newTotal = true;
-}
 
 
 
